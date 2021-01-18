b4bab7611a4854bfedebe19fb456c3e9db5994f3
Nilber Remon <nilberremon@gmail.com>
2021-01-18 16:53:49 -0500

    GraphQL Config

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,35 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config"
+import express from 'express'
+import {ApolloServer} from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => { //In a 'main' function to access/use async/await
 
     //Connect to database
     const orm = await MikroORM.init(microConfig); //method calls/returns a promise 
-    
     //Run migrations
     await orm.getMigrator().up();
+
+    const app = express();
+
+    
+    
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({ //buildSchema returns promise
+            resolvers: [HelloResolver],
+            validate: false, //uses Class package as a validator, here turning it off
+
+        }) 
+    })
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log('Server Party @4000')
+    })
 
     //Run SQL
     //const post = orm.em.create(Post, {title: 'My First Post 1'})
