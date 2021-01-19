a0b7863315278a0164f97b10cb50f78cc83d0882
Nilber Remon <nilberremon@gmail.com>
2021-01-18 22:42:34 -0500

    User GraphQL Resolvers

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -41,6 +41,32 @@ __decorate([
 UsernamePasswordInput = __decorate([
     type_graphql_1.InputType()
 ], UsernamePasswordInput);
+let FieldError = class FieldError {
+};
+__decorate([
+    type_graphql_1.Field(),
+    __metadata("design:type", String)
+], FieldError.prototype, "field", void 0);
+__decorate([
+    type_graphql_1.Field(),
+    __metadata("design:type", String)
+], FieldError.prototype, "message", void 0);
+FieldError = __decorate([
+    type_graphql_1.ObjectType()
+], FieldError);
+let UserResponse = class UserResponse {
+};
+__decorate([
+    type_graphql_1.Field(() => [FieldError], { nullable: true }),
+    __metadata("design:type", Array)
+], UserResponse.prototype, "errors", void 0);
+__decorate([
+    type_graphql_1.Field(() => User_1.User, { nullable: true }),
+    __metadata("design:type", User_1.User)
+], UserResponse.prototype, "user", void 0);
+UserResponse = __decorate([
+    type_graphql_1.ObjectType()
+], UserResponse);
 let UserResolver = class UserResolver {
     register(options, { em }) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -53,6 +79,35 @@ let UserResolver = class UserResolver {
             return user;
         });
     }
+    login(options, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const user = yield em.findOne(User_1.User, { username: options.username });
+            if (!user) {
+                return {
+                    errors: [
+                        {
+                            field: "username",
+                            message: "User Doesn't Exist"
+                        },
+                    ]
+                };
+            }
+            const valid = yield argon2_1.default.verify(user.password, options.password);
+            if (!valid) {
+                return {
+                    errors: [
+                        {
+                            field: "password",
+                            message: "Incorrect Password"
+                        },
+                    ]
+                };
+            }
+            return {
+                user
+            };
+        });
+    }
 };
 __decorate([
     type_graphql_1.Mutation(() => User_1.User),
@@ -62,6 +117,14 @@ __decorate([
     __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
     __metadata("design:returntype", Promise)
 ], UserResolver.prototype, "register", null);
+__decorate([
+    type_graphql_1.Mutation(() => UserResponse),
+    __param(0, type_graphql_1.Arg('options')),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
+    __metadata("design:returntype", Promise)
+], UserResolver.prototype, "login", null);
 UserResolver = __decorate([
     type_graphql_1.Resolver()
 ], UserResolver);
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,9 +1,9 @@
 import argon2 from 'argon2'
 import { User } from '../entities/User'
 import { MyContext } from 'src/types'
-import {Arg, Ctx, Field, InputType, Mutation, Resolver} from 'type-graphql'
+import {Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver} from 'type-graphql'
 
-@InputType()
+@InputType() //arguments in mutations
 class UsernamePasswordInput {
     @Field(() => String)
     username: string
@@ -11,12 +11,29 @@ class UsernamePasswordInput {
     password: string
 }
 
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string
+    @Field()
+    message: string
+}
+
+@ObjectType() //for returning objects from mutations
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[] //? - can be undefined
+
+    @Field(() => User, {nullable: true})
+    user?: User
+}
+
 @Resolver()
 export class UserResolver {
     
     @Mutation(() => User)
     async register(
-        @Arg('options') options: UsernamePasswordInput,
+        @Arg('options') options: UsernamePasswordInput, //Don't need to specify Arg type, inferred as () => UsernamePasswordInput
         @Ctx() { em }: MyContext
 
     ) {
@@ -28,5 +45,42 @@ export class UserResolver {
         await em.persistAndFlush(user)
         return user
     }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, {username: options.username})
+        if(!user) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "User Doesn't Exist"
+                    },
+                ]
+            }
+        }
+
+        const valid = await argon2.verify(user.password, options.password)
+        if(!valid) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "Incorrect Password"
+                    },
+                ]
+            
+            }
+        
+        }
+
+        return {
+            user
+        }
+    }
+
 
 }
