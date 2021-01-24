8670ce3bca565c2035b0146663310419b8e80287
Nilber Remon <nilberremon@gmail.com>
2021-01-23 23:51:20 -0500

    Context Types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
-import { Request, Response } from 'express'
+import { Request, Response } from "express";
 
 export type MyContext = {
     em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-    req: Request & {session: Express.Session};
+    req: Request & { session: Express.session };
     res: Response;
 }
